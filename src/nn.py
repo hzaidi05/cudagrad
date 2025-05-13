f10e2d49e21aec10a715356d8da97e1ba4e848e0
Hamza Zaidi <cookiestein3@gmail.com>
2025-05-13 17:50:17 -0400

    naive port to use ND arrays and support GPU backend

--- src/nn.py
+++ src/nn.py
@@ -1,31 +1,28 @@
 import random
-from micrograd.engine import Value
+import numpy as np
+import cupy as cp
+from tensor import Tensor
 
 class Module:
-
-    def zero_grad(self):
-        for p in self.parameters():
-            p.grad = 0
-
     def parameters(self):
         return []
 
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = cp.zeros_like(p.data) if p.device == 'GPU' else np.zeros_like(p.data)
+
 class Neuron(Module):
-
-    def __init__(self, nin, nonlin=True):
-        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
-        self.b = Value(0)
-        self.nonlin = nonlin
-
+    def __init__(self, nin, device='CPU'):
+        self.device = device
+        self.w = Tensor(np.random.randn(nin, 1) * 0.1, device=device)
+        self.b = Tensor(np.zeros((1, 1)), device=device)
+    
     def __call__(self, x):
-        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
-        return act.relu() if self.nonlin else act
-
+        out = self.w.T @ x + self.b  # Supports batched inputs
+        return out.relu()
+    
     def parameters(self):
-        return self.w + [self.b]
-
-    def __repr__(self):
-        return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+        return [self.w, self.b]
 
 class Layer(Module):
 
@@ -39,22 +36,23 @@ class Layer(Module):
     def parameters(self):
         return [p for n in self.neurons for p in n.parameters()]
 
-    def __repr__(self):
-        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
 class MLP(Module):
-
-    def __init__(self, nin, nouts):
-        sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
-
+    def __init__(self, nin, nouts, device='CPU'):
+        sizes = [nin] + nouts
+        self.layers = [Neuron(sizes[i], device=device) for i in range(len(nouts))]
+    
     def __call__(self, x):
         for layer in self.layers:
             x = layer(x)
         return x
-
+    
     def parameters(self):
         return [p for layer in self.layers for p in layer.parameters()]
 
-    def __repr__(self):
-        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
+# Example Usage
+if __name__ == "__main__":
+    x = Tensor(np.random.randn(3, 2), device='GPU')  # Batch of 3 samples, 2 features each
+    model = MLP(2, [3, 1], device='GPU')
+    y = model(x)
+    print("y.data:", y.data)
--- src/tensor.py
+++ src/tensor.py
@@ -1,94 +1,80 @@
+import numpy as np
+import cupy as cp
 
-class Value:
-    """ stores a single scalar value and its gradient """
-
-    def __init__(self, data, _children=(), _op=''):
-        self.data = data
-        self.grad = 0
-        # internal variables used for autograd graph construction
+class Tensor:
+    def __init__(self, data, device='CPU', requires_grad=True):
+        if device == 'GPU':
+            self.data = cp.array(data, dtype=cp.float32)
+        else:
+            self.data = np.array(data, dtype=np.float32)
+        
+        self.grad = cp.zeros_like(self.data) if device == 'GPU' else np.zeros_like(self.data)
+        self.requires_grad = requires_grad
+        self.device = device
         self._backward = lambda: None
-        self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._prev = set()
 
     def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
-
+        other = other if isinstance(other, Tensor) else Tensor(other, device=self.device)
+        out = Tensor(self.data + other.data, device=self.device)
+        
         def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
+            grad_self = out.grad
+            grad_other = out.grad
+            if self.data.shape != out.data.shape:
+                grad_self = grad_self.sum(axis=tuple(range(grad_self.ndim - self.data.ndim)), keepdims=True)
+            if other.data.shape != out.data.shape:
+                grad_other = grad_other.sum(axis=tuple(range(grad_other.ndim - other.data.ndim)), keepdims=True)
+            self.grad += grad_self
+            other.grad += grad_other
         out._backward = _backward
-
+        out._prev = {self, other}
         return out
 
     def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, (self, other), '*')
-
+        other = other if isinstance(other, Tensor) else Tensor(other, device=self.device)
+        out = Tensor(self.data * other.data, device=self.device)
+        
         def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
+            grad_self = other.data * out.grad
+            grad_other = self.data * out.grad
+            if self.data.shape != out.data.shape:
+                grad_self = grad_self.sum(axis=tuple(range(grad_self.ndim - self.data.ndim)), keepdims=True)
+            if other.data.shape != out.data.shape:
+                grad_other = grad_other.sum(axis=tuple(range(grad_other.ndim - other.data.ndim)), keepdims=True)
+            self.grad += grad_self
+            other.grad += grad_other
         out._backward = _backward
-
+        out._prev = {self, other}
         return out
 
-    def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Value(self.data**other, (self,), f'**{other}')
-
+    def __matmul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other, device=self.device)
+        
+        if self.device == 'GPU':
+            #todo
+        else:
+            out = Tensor(self.data @ other.data, device='CPU')
+        
         def _backward():
-            self.grad += (other * self.data**(other-1)) * out.grad
+            self.grad += out.grad @ other.data.T
+            other.grad += self.data.T @ out.grad
         out._backward = _backward
-
+        out._prev = {self, other}
         return out
-
-    def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
-
-        def _backward():
-            self.grad += (out.data > 0) * out.grad
-        out._backward = _backward
-
-        return out
-
+    
     def backward(self):
-
-        # topological order all of the children in the graph
         topo = []
         visited = set()
+        
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
-                for child in v._prev:
-                    build_topo(child)
+                for parent in v._prev:
+                    build_topo(parent)
                 topo.append(v)
+        
         build_topo(self)
-
-        # go one variable at a time and apply the chain rule to get its gradient
-        self.grad = 1
+        self.grad = cp.ones_like(self.data) if self.device == 'GPU' else np.ones_like(self.data)
         for v in reversed(topo):
             v._backward()
-
-    def __neg__(self): # -self
-        return self * -1
-
-    def __radd__(self, other): # other + self
-        return self + other
-
-    def __sub__(self, other): # self - other
-        return self + (-other)
-
-    def __rsub__(self, other): # other - self
-        return other + (-self)
-
-    def __rmul__(self, other): # other * self
-        return self * other
-
-    def __truediv__(self, other): # self / other
-        return self * other**-1
-
-    def __rtruediv__(self, other): # other / self
-        return other * self**-1
-
-    def __repr__(self):
-        return f"Value(data={self.data}, grad={self.grad})"
